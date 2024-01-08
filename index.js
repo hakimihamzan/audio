b959a8489942922cfdaa4482b65f1041ac798c68
hakimihamzan <hakimihamzan20@gmail.com>
2024-01-08 22:50:56 +0800

    cleanup

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ function play(e, id) {
     let audio = document.getElementById(id);
 
     if (!playButton.dataset.play || playButton.dataset.play == 'tidak') {
-        playButton.dataset.play = 'ya';
-        playButton.src = 'assets/stop.png';
+        playButton.dataset.play = 'ya'; // this data attribute is used to detect whether the player is playing or not
+        playButton.src = 'assets/stop.png'; // change image to stop icon
         audio.play();
     } else {
-        playButton.dataset.play = 'tidak';
-        playButton.src = 'assets/play.png';
+        playButton.dataset.play = 'tidak'; // this data attribute is used to detect whether the player is playing or not
+        playButton.src = 'assets/play.png'; // change image to play icon
         audio.currentTime = 0;
         audio.pause();
     }
